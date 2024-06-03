f8bad047c57ddf1c33bc5a9ed603ea5338a2a862
Elduro687 <66131760+Elduro687@users.noreply.github.com>
2024-06-03 18:04:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ let turnCount = 1;
 let totalTurnCount = 0;
 
 const turnImages = [
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_0.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_1.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_2.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_3.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_4.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_5.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_6.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_7.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_8.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_9.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_10.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_11.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_12.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_13.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_14.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_15.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_16.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_17.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_18.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_19.jpg",
-    "/Users/sergioruiz/Desktop/F1sim/turns/monaco_turn_20.jpg"
+    ".../turns/monaco_turn_0.jpg",
+    ".../monaco_turn_1.jpg",
+    ".../turns/monaco_turn_2.jpg",
+    ".../turns/monaco_turn_3.jpg",
+    ".../turns/monaco_turn_4.jpg",
+    ".../turns/monaco_turn_5.jpg",
+    ".../turns/monaco_turn_6.jpg",
+    ".../turns/monaco_turn_7.jpg",
+    ".../turns/monaco_turn_8.jpg",
+    "...turns/monaco_turn_9.jpg",
+    ".../turns/monaco_turn_10.jpg",
+    ".../turns/monaco_turn_11.jpg",
+    "...turns/monaco_turn_12.jpg",
+    ".../turns/monaco_turn_13.jpg",
+    ".../turns/monaco_turn_14.jpg",
+    ".../turns/monaco_turn_15.jpg",
+    ".../turns/monaco_turn_16.jpg",
+    ".../turns/monaco_turn_17.jpg",
+    ".../turns/monaco_turn_18.jpg",
+    ".../turns/monaco_turn_19.jpg",
+    ".../turns/monaco_turn_20.jpg"
 ];
 
 moveCarButton.addEventListener('click', () => {
